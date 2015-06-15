6725b61d9360ecf15965efd7a53af9562bef2ae6
walkeranthonyp <walkeranthonyp@gmail.com>
2015-06-15 18:15:26 -0400

    update map functions

--- tools/functions_map_plot.R
+++ tools/functions_map_plot.R
@@ -3,15 +3,24 @@
 # global plotting function
 #
 # AWalker
-# Feb 2012
+# Jun 2015
 #
 ###################################
 
-library(maps)
-library(fBasics)
+#library(maps)
+#library(fBasics)
 
+.libPaths('~/bin/Rlibs')
+library(lattice)
+library(latticeExtra)
+library(rworldmap)
+library(rworldxtra)
+
+  
+  
 ###############################
-#open IMOGEN-SDGVM / annual data SDGVM files
+#open annual data SDGVM files
+
 open <- function(var,mnames,wdpath){
   ifile <- paste(var,'.dat',sep='')
   print(ifile)
@@ -20,8 +29,10 @@ open <- function(var,mnames,wdpath){
   x
 }
 
+  
 
 ###############################
+
 grid_calc <- function(x,gsum,index,res){        
   
   # res 1 - longitude resolution
@@ -54,91 +65,20 @@ grid_calc <- function(x,gsum,index,res){
   
   #return processed dataframe
   x
-  
 }
 
+  
+  
 ###############################
-scalef <- function(x,scalemin,scalemax,ncolours){
-  #calculate subscript for colour array 
-  
-  range <- scalemax-scalemin
-  xcorr <- x-scalemin+0.0001
-  i     <- xcorr*ncolours/range
-  i     <- ceiling(i)+1
-  adj_ncolours <- ncolours+2
-  
-  #adjust for out of bounds values
-  ifelse(is.na(x)|x<=0,i<-1,
-         ifelse(i>ncolours+1,i<-adj_ncolours,ifelse(i<1,
-                                                    i<-1,i<-i)))
-  
-  #returns
-  i  
-}
 
-###############################
-write_title  <- function(mod,vn,vs) as.expression(substitute(list(mod*' '*vname[vsub]),list(mod=mod,vname=vn,vsub=vs)))
-
-###############################
+write_title <- function(mod,vn,vs) as.expression(substitute(list(mod*' '*vname[vsub]),list(mod=mod,vname=vn,vsub=vs)))
 write_var   <- function(vn,vs) as.expression(substitute(list(vname[vsub]),list(vname=vn,vsub=vs)))
 
-###############################
-###scale plotting function
-# colours<-colv[[colo]]
-# scalemin<-scaleminv[v]
-# scalemax<-scalemaxv[v]
 
-plot_scale<-function(scalemin,scalemax,colours,r,sfactor,sunit,vname,vsub){
-  #plots a horizontal scale at the bottom of the figure 
-  
-  cex <- 0.7
-  
-  #add out of range high colour
-  colours  <- c(colours,'red')
-  ncolours <- length(colours)
-  
-  range0   <- scalemax-scalemin
-  
-  scalemax0<- scalemax
-  scalemax <- scalemax+range0/(ncolours-1)
-  range    <- scalemax-scalemin
-  
-  par(mar=c(0,7,0,7))
-  plot(c(scalemin,scalemax),c(-2,4),type="n", ann=FALSE, axes=FALSE)
-  
-  y1<-c(0,1,1,0)
-  y1<-y1+0.2
-  xs<-scalemin
-  xi<-range/ncolours
-  xa<-xs
-  xb<-xa+xi
-  
-  for(c in 1:length(colours)){
-    x1<-c(xa,xa,xb,xb)
-    polygon(x1,y1,col=colours[c],border='black')
-    xa<-xb
-    xb<-xa+xi
-  }
-  
-  text(scalemin,-1,as.character(round(scalemin,r)),cex=cex)
-  text(scalemax0,-1,paste('>',as.character(round(scalemax0,r)),sep=''),cex=cex)
-   
-  nticks<-(scalemax0-scalemin)/sfactor-1
-  
-  if(scalemin>=0){
-    for(si in 1:nticks){
-      x<-xi*si+scalemin
-      text(x,-1,as.character(round(x,r)),cex=cex)
-    }}
-  
-  text(scalemin,2.4,write_var(vname,vsub),pos=4,cex=cex*1.5)
-  text((scalemin+range*0.2),2.4,parse(text=sunit),pos=4,cex=cex*1.5)
-  
-  #end function
-}   
 
 ###############################
-region<-function(x,region){
+
+region <- function(x,region){
   #subset dataset 'x' by region 
   
   # returns 'xylims' a 6 element vector composing of:
@@ -177,12 +117,10 @@ region<-function(x,region){
 }
 
 
+
 ###############################   
 #integrate per m values to the total amount for the globe/region
-# x <- mydata[,1:2]
-# y <- mydata[,3:length(mydata)]
-# res<-mod_res
-# gm<-gmean[v]
+
 area_integrate <- function(x,y,res,gm){
   # x is a dataframe with variables 'lat' , 'lon'
   # y is a dataframe of the actual data, n columns the same as the columns of the output  
@@ -202,96 +140,9 @@ area_integrate <- function(x,y,res,gm){
 
 
 
-###############################   
-#map plotting function
-# v<-1
-# variable<-vars[v]
-# scalemin<-scaleminv[v]
-# scalemax<-scalemaxv[v]
-# sfactor<-sfactor[v]
-# vname<-vname[v]
-# varsub<-vsub[v]
-# sunit<-sunit[v]
-# colo<-colo[v]
-# gsum<-gsum[v]
-# unit<-unit[v]
-# colv<-colv
-# x<-subdata
-plot_map <- function(x,index,scalemin,scalemax,sfactor,vname,varsub,sunit,colo,gsum,unit,colv,pregion='global',model,year,mar){
-#rl is the last column in the data frame before the ploygon columns
-#could add polygon calculation to this function 
-#index is the column with the data to be plotted
-
-  #mar=c(0.5,2,0.5,0)
-    
-  rl      <- length(x) - 11
-  print(rl)
-  cl      <- length(x[,1])  
-  ncol    <- length(colv[[colo]])
-  colours <- c('white',colv[[colo]],'red')
-  
-  ifelse(gsum, 
-         global_sum<-round(sum(x$sum,na.rm=T)*1e-9,1),
-         global_sum<-round(sum(x$sum,na.rm=T),2)
-  )
-  
-  #write lat lon -90-90 & -180-180
-  date_line<-min(x$e_bound[which(abs(x$e_bound-180)==min(abs(x$e_bound-180)))])
-  x$e_bound[x$e_bound>date_line]  <- x$e_bound[x$e_bound>date_line]-360
-  x$w_bound[x$w_bound>=date_line] <- x$w_bound[x$w_bound>=date_line]-360
-
-  #subset dataset 
-  #remove nas
-  x <- x[is.finite(x[,index]),]  
-  #by region
-  lims  <- region(x,pregion)
-  #subset data to region
-  x <- subset(x,w_bound>lims[1])
-  x <- subset(x,e_bound<lims[2])
-  x <- subset(x,s_bound>lims[3])
-  x <- subset(x,n_bound<lims[4])
-#   print(head(x))
-#   lims <- c(-90,-30,-30,15)
-#   lims <- c(-90,180,-30,30)
-    
-  #plot
-#   plot(getMap('high'),lwd=0.5,xlim=c(-180,180),ylim=c(-60,90))
-  map("world", xlim=c(lims[1],lims[2]), ylim=c(lims[3],lims[4]), interior=T, bg="white", col="black",cex=1,mar=mar)
-  axis(2,at=seq(lims[3],lims[4],lims[6]),pos=lims[1],las=TRUE) 
-  axis(1,at=seq(lims[1],lims[2],lims[5]),pos=lims[3]) 
-  axis(3,labels=F,tck=0,pos=lims[4],at=c(lims[1],lims[2]))
-  axis(4,labels=F,tck=0,pos=lims[2],at=c(lims[3],lims[4]))
-
-  title(main=write_title(paste(model,year),vname,varsub))
-  
-  print(rl);print(length(x));print(cl)
-  for (i in (1:cl)){
-    polygon(x[i,(rl+1):(rl+4)], x[i,(rl+5):(rl+8)], col=colours[scalef(x[i,index],scalemin,scalemax,ncol)],border=NA)
-  }
-  
-#   plot(getMap('high'),add=T,lwd=0.5,xlim=c(-180,180),ylim=c(-60,90))
-  map("world",  xlim=c(lims[1],lims[2]), ylim=c(lims[3],lims[4]), interior=T, bg="white", col="black",add=T,mar=mar)
-  text(x=-135,y=-45,labels=ifelse(gsum|gmean,paste(global_sum,unit),''))
-  
-  abline(h=0,lty=2,lwd=0.6)
-  abline(h=c(-23.3,23.3,66.5),lty=3,lwd=0.6)
-}   
-   
-
-
 ### Lattice map plotting functions
-#rworldmap mods
+# rworldmap mods
 ###############################
-# dev.off()
-# mapGriddedData(mapRegion='Africa')
-# mapGriddedData
-# rmw <- rworldmap:::rwmNewMapPlot
-# rworldmap:::setMapExtents
- 
-library(lattice)
-library(latticeExtra)
-library(rworldmap)
-library(rworldxtra)
 
 plotmap <- function(i,map) {
   pm <- function(j,i,pg){
@@ -300,6 +151,8 @@ plotmap <- function(i,map) {
   polys <- map@polygons[[i]]@Polygons
   lapply(1:length(polys),pm,i=i,pg=polys)
 }
+
+
 
 plot_map_lattice <- function(x,index,lab,pregion='global',model,
                              year,mar,gs=NULL,...){
@@ -353,8 +206,9 @@ plot_map_lattice <- function(x,index,lab,pregion='global',model,
             }) #+ layer_(panel.2dsmoother(..., n = 200))
 }   
 
-axis.ticks <- function (...,y=F,ticks = seq(-180,180,20), ticks2 = seq(-180,180,5)) 
-{
+
+
+axis.ticks <- function (...,y=F,ticks = seq(-180,180,20), ticks2 = seq(-180,180,5)){
   ans    <- xscale.components.default(...)
   ticks2 <- ticks2[!(ticks2 %in% ticks)]
   ans$bottom$ticks$at  <- c(ticks, ticks2)
@@ -373,12 +227,4 @@ axis.ticks <- function (...,y=F,ticks = seq(-180,180,20), ticks2 = seq(-180,180,
   }
   ans
 }
-
-# xyplot(1:10~1:10,
-#        panel=function(...){
-#          panel.xyplot(...)
-#          panel.text(x=5,y=5,'text')
-#          panel.text(x=8,y=5,parse(text='mu*mol'))
-#        })
-
 
--- tools/plot_maps_lattice.R
+++ tools/plot_maps_lattice.R
@@ -3,92 +3,90 @@
 # global plotting function
 #
 # AWalker
-# Feb 2012
+# Jun 2015
 #
 ###################################
 
 rm(list=ls())
 
-setwd('/mnt/disk2/models/SDGVM/tools/')
-source('params_map.R')
-setwd('/mnt/disk2/script_library/R/map_tools/')
-source('map_plot.R')
-
+.libPaths('~/bin/Rlibs')
 library(lattice)
 library(plyr)
+
+
+
 
 ##################################
 ###user defined inputs
 
 #directory paths
-date    <- '150302'
-wd      <- '/mnt/disk2/Research_Projects/leaf_trait_SDGVM/'
-trends  <- T
-deg1    <- T
+date    <- '150615'
+wd      <- '/home/alp/models/SDGVM/'
+rdir    <- 'run'
 
-project <- 'TERRABITES'
-sim     <- c('T1_131001_GLC2000','T1_131001_GLOB2009','T1_140129','T1_noC4','T1','T2','T3')
-sia     <- c(1:7)
-sia     <- 5:7
-# sia     <- 3:4
+# project directory
+project <- 'vcmax'
+
+# simulations
+sim     <- c('original','orig_N','Kattge','Kattge_oxisol','Maire','vBodegom_env','vBodegom_mean','Walker_N','Walker_NP','Woodward_94','Woodward_95')
+
+# simulations index array (which simulations to plot)
+sia     <- 1:length(sim)
+
+# output file naming prefix
 of      <- project
 
-# project <- 'sub-daily_PAR_test'
-# sim     <- c('one','five','ten','twenty','thirty')
-# sim     <- c('one','five','ten','thirteen','sixteen','twenty','thirty')
-# 
-# project <- 'SDGVM_light'
-# sim     <- c('orig','orig_corr','orig_PAR','orig_Vcmax','varPAR','varPAR_Cl','varPAR_Cln','xPAR','xPAR_Cl','xPAR_Cln')
-# of      <- 'SDGVMdev'
-# sia <- 1:4
-# #of      <- 'SDGVMnew'
-# #sia     <- c(8,5,9,6,10,7)
-# # sia     <- c(9,6) 
-# # sia     <- 5
-#
-# project <- 'SDGVM_light_140928'
-# sim     <- c('orig','orig_corr','orig_PAR','orig_Vcmax','new','new_vm_maire','new_vm_orig1995','new_vm_P','new_C','new_vm_maire_Cl','new_vm_orig1995_Cl','new_vm_P_Cl')
-# of      <- 'SDGVMdev'
-# sia <- 6
-# #sia     <- 5:length(sim)
-# #sia     <- 1:4
- 
-owd     <- paste(wd,'results',project,date,sep='/')
+# decimal places for scale legend
+r <- 2
+
+# map data resolution  
+deg1    <- T
+
+# data start year
+styr    <- 1901
+
+# data end year
+endyr   <- 2010
+
+# plotting region
+pregion <- 'global'
+
+# plot year (can be a vector of years)
+year    <- 2012
+
+# plot trends across whole timeseries
+trends  <- T
+
+# plotting variables (these all have an associated list in 
+vars <- c('npp','gpp','nbp',
+          'anlfn','antlfn',        
+          'evt','trn','scn','sresp','presp','mgresp',
+          'lai','anvcmax','anjmax','biot','kg_beta','swr','qtotal',
+          'tmp','prc','swc','field_capacity','wilting_point',
+          'cov_C3','cov_C4','cov_C3crop','cov_C4crop','cov_Dc_Bl','cov_Dc_Nl','cov_Ev_Bl','cov_Ev_Nl','cov_BARE')
+
+# variable index array (which variables to plot)
+via <- 1:length(vars)
+via <- 2
+
+
+###############################
+### start program
+
+setwd(paste(wd,'src/sdgvm/tools/',sep='/'))
+source('params_map_plot.R')
+source('functions_map_plot.R')
+
+         mod_res <- c(3.75,2.5)
+if(deg1) mod_res <- c(1,1)
+
+# create output directory
+owd     <- paste(wd,rdir,project,'results',date,sep='/')
 if(!file.exists(owd)) dir.create(owd)
 
 
-# year/s to plot
-year  <- c(1990,2000,2010)
-#year  <- c(2009)
-styr  <- 1901
-endyr <- 2010
 
-# variables
-mod_res <- c(3.75,2.5)
-if(deg1) mod_res <- c(1,1)
-pregion <- 'global'
-
-vars<-c('npp','gpp','nbp',
-        'anlfn','antlfn',        
-        'evt','trn','scn','sresp','presp','mgresp',
-        'lai','anvcmax','anjmax','biot','kg_beta','swr','qtotal',
-        'tmp','prc','swc','field_capacity','wilting_point',
-        'cov_C3','cov_C4','cov_C3crop','cov_C4crop','cov_Dc_Bl','cov_Dc_Nl','cov_Ev_Bl','cov_Ev_Nl','cov_BARE')
-
-#variable index array
-via <- 1:length(vars)
-# via <- (length(vars)-8):length(vars)
-# via <- 1:21
-# via <- 2
-# via <- 13:15
-
-###############################
-###start program
-
-#decimal places for scale legend
-r<-2
-# variable loop
-v   <- 2
+# variable plotting loop
 for(v in via){
   lab <- get(vars[v])
 
@@ -103,11 +101,9 @@ for(v in via){
   # rs <- 3
   
   # data loop
-  m <- 1
   for(m in sia){    
     #open data 
-    wdpath  <- paste(wd,'simulations',project,sim[m],'output/',sep='/')
-    if(deg1) wdpath  <- paste(wd,'simulations',project,sim[m],'output_1deg/',sep='/')
+    wdpath  <- paste(wd,rdir,project,sim[m],'output/',sep='/')
     print(wdpath)
     
     #mydata column to plot
@@ -160,10 +156,10 @@ for(v in via){
   trellis.par.set(mpars)
   print(p1)
   dev.off()  
-#   pdf(pdfofile,width=18,height=12,pointsize=28)
-#   trellis.par.set(mpars)
-#   print(p1)
-#   dev.off()  
+  pdf(pdfofile,width=18,height=12,pointsize=28)
+  trellis.par.set(mpars)
+  print(p1)
+  dev.off()  
   
   
   #plot trends
