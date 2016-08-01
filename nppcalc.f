9ac974397b35299da77be838730f54d1df85a1ce
AnthonyWalker <walkeranthonyp@gmail.com>
2016-08-01 16:31:37 -0400

    revert to Farquhar temp scaling & previous alpha

--- nppcalc.f
+++ nppcalc.f
@@ -101,18 +101,18 @@
       nleaf(:)  = 0.0d0
 
       ! below are the kinetic parameters from Farquhar etal 1980
-      !kc  = exp(35.8d0 - 80.5d0/(0.00831d0*tk))
-      !ko  = exp(9.6d0 - 14.51d0/(0.00831d0*tk))*1000.0d0
-      !tau = exp(-3.949d0 + 28.99d0/(0.00831d0*tk))
+      kc  = exp(35.8d0 - 80.5d0/(0.00831d0*tk))
+      ko  = exp(9.6d0 - 14.51d0/(0.00831d0*tk))*1000.0d0
+      tau = exp(-3.949d0 + 28.99d0/(0.00831d0*tk))
 
-      ! the above are now deprecated for the in vivo parameters froim Bernacchi etal 2001
-      kc  = 40.49d0 * exp((79430.0d0 / (8.31d0 * 
-     &298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
-      ko  = 27840.d0 * exp((36380.0d0 / (8.31d0 *
-     &298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
-      c_p = 4.275d0 * exp((37830.0d0 / (8.31d0 * 
-     &298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
-      tau = 0.5d0*oi/c_p
+      ! the above are now deprecated for the in vivo parameters from Bernacchi etal 2001
+      !kc  = 40.49d0 * exp((79430.0d0 / (8.31d0 * 
+      !&298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
+      !ko  = 27840.d0 * exp((36380.0d0 / (8.31d0 *
+      !&298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
+      !c_p = 4.275d0 * exp((37830.0d0 / (8.31d0 * 
+      !&298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
+      !tau = 0.5d0*oi/c_p
 
       ! changed by Ghislain 08/10/03      IF (rlai.GT.0.1d0) THEN
       q = qdiff + qdirect
@@ -742,7 +742,8 @@
          ! and is the value used by Harley,  
          ! however Goudriaan's Law returns absorbed light so already accounts for reflection and transmission
          ! so the intrinsic quantum efficiency should be used, the 1.15 multiplier in the below function accounts for this
-         j=1.15d0*0.24d0*q/(1.0d0+(0.24d0**2)*(q**2)/(jmx**2))**0.5d0
+         !j=1.15d0*0.24d0*q/(1.0d0+(0.24d0**2)*(q**2)/(jmx**2))**0.5d0
+         j=0.24d0*q/(1.0d0+(0.24d0**2)*(q**2)/(jmx**2))**0.5d0
       ELSE
          !use Farquhar & Wong 1984 J to Jmax realtionship
          I2 = q * 0.36d0    
@@ -1301,21 +1302,22 @@
       oi = 21000.0d0 
 
       ! see explanation for 1.15 multiplier in subroutine JCALC       
-      alpha = 1.15d0*0.24d0
+      !alpha = 1.15d0*0.24d0
+      alpha = 0.24d0
       
       ! below are the kinetic parameters from Farquhar etal 1980
-      !kc  = exp(35.8d0 - 80.5d0/(0.00831d0*tk))
-      !ko  = exp(9.6d0 - 14.51d0/(0.00831d0*tk))*1000.0d0
-      !tau = exp(-3.949d0 + 28.99d0/(0.00831d0*tk))
+      kc  = exp(35.8d0 - 80.5d0/(0.00831d0*tk))
+      ko  = exp(9.6d0 - 14.51d0/(0.00831d0*tk))*1000.0d0
+      tau = exp(-3.949d0 + 28.99d0/(0.00831d0*tk))
 
       ! the above are now deprecated for the in vivo parameters from Bernacchi etal 2001
-      kc  = 40.49d0 * exp((79430.0d0 / (8.31d0 * 
-     &298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
-      ko  = 27840.d0 * exp((36380.0d0 / (8.31d0 *
-     &298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
-      c_p = 4.275d0 * exp((37830.0d0 / (8.31d0 * 
-     &298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
-      tau = 0.5d0*oi/c_p
+      !kc  = 40.49d0 * exp((79430.0d0 / (8.31d0 * 
+      !&298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
+      !ko  = 27840.d0 * exp((36380.0d0 / (8.31d0 *
+      !&298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
+      !c_p = 4.275d0 * exp((37830.0d0 / (8.31d0 * 
+      !&298.15d0)) * (1.0d0 - (298.15d0) / (273.15d0 + t)))
+      !tau = 0.5d0*oi/c_p
 
       END SUBROUTINE
 *----------------------------------------------------------------------*
