bee9cb903729b10c03f6b0bd8db081b5ce8643a0
Patrick McGuire <p.mcguire@reading.ac.uk>
2023-08-16 22:24:58 +0100

    updated debugging site number

--- sdgvm0.f
+++ sdgvm0.f
@@ -1972,7 +1972,7 @@ c CLOSE added by Ghislain 15/12/03
 *----------------------------------------------------------------------*
 !PCM4 for running a single site in the main run from a gridded spinup;
 !PCM4 (other changes below)
-!      DO site=10,10
+!      DO site=28,28
       DO site=1,sites
 *----------------------------------------------------------------------*
 * closed_loop_ft:                                                      *
