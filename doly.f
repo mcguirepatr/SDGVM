dcddf103a03ac7254eb115514e2634389bf5781e
Patrick McGuire <p.mcguire@reading.ac.uk>
2026-07-30 14:50:34 +0100

    made the wind file optional

--- doly.f
+++ doly.f
@@ -228,8 +228,12 @@ c     z=reference height
 c     d=zero plane displacement
 c     z0=roughness length
 
-!      windspeed= 5.0d0 ! in m/s
-      windspeed= wnd ! in m/s !PCM
+C      windspeed= 5.0d0 ! in m/s
+
+C      This is set to 5 m/s in data.f if the wind file doesn't exist. 
+C         Otherwise, if the wind file exists, the wind values are read
+C         in from there.
+       windspeed= wnd ! in m/s !PCM
 C      WRITE(*,*) 'windspeed = ',windspeed
 
 C PCM      canga = 0.168d0*windspeed/log((200.0d0 - 
